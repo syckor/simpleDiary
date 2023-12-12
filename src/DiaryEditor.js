71bfd2f9a8d10323be539431361c82c533f8f996
syckor <syckor@naver.com>
2023-12-12 23:25:21 +0900

    create select box

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -5,8 +5,9 @@ import  {useState} from "react";
 const DiaryEditor = () => {
 
     const [state, setState] = useState({
-        author:"",
-        content:""
+        author: "",
+        content: "",
+        emotion: 1,
     })
 
     const handleChangeState = (e) => {
@@ -32,6 +33,15 @@ const DiaryEditor = () => {
                     value={state.content} 
                     onChange={handleChangeState}
                 />
+            </div>
+            <div>
+                <select name='emotion' value={state.emotion} onChange={handleChangeState}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                </select>
             </div>
         </div>
     );
