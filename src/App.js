8001c39dff22692141c43f6a0161faf3550258f2
syckor <syckor@naver.com>
2023-12-13 23:31:30 +0900

    일기 내용 쓰고 리스트에 추가하기

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 
 import './App.css';
+import  {useRef, useState} from "react";
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [
-  {
-    id:1,
-    author:"이정환",
-    content:"하이 1",
-    emotion:5,
-    created_date: new Date().getTime()
-  },{
-    id:2,
-    author:"홍길동",
-    content:"하이 2",
-    emotion:3,
-    created_date: new Date().getTime()
-  },{
-    id:3,
-    author:"서영철",
-    content:"하이룽 1",
-    emotion:1,
-    created_date: new Date().getTime()
-  },{
-    id:4,
-    author:"전예슬",
-    content:"하이롱롱롱 1",
-    emotion:2,
-    created_date: new Date().getTime()
-  },
-]
+// const dummyList = [
+//   {
+//     id:1,
+//     author:"이정환",
+//     content:"하이 1",
+//     emotion:5,
+//     created_date: new Date().getTime()
+//   },{
+//     id:2,
+//     author:"홍길동",
+//     content:"하이 2",
+//     emotion:3,
+//     created_date: new Date().getTime()
+//   },{
+//     id:3,
+//     author:"서영철",
+//     content:"하이룽 1",
+//     emotion:1,
+//     created_date: new Date().getTime()
+//   },{
+//     id:4,
+//     author:"전예슬",
+//     content:"하이롱롱롱 1",
+//     emotion:2,
+//     created_date: new Date().getTime()
+//   },
+// ]
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem={
+      author, content, emotion, created_date, id: dataId.current
+    }
+    dataId.current += 1;
+    setData([newItem, ...data] );
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <DiaryEditor/>
-        <DiaryList diaryList={dummyList} />
+        <DiaryEditor onCreate={onCreate}/>
+        <DiaryList diaryList={data} />
       </header>
     </div>
   );
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -2,7 +2,7 @@ import  {useRef, useState} from "react";
 
 
 
-const DiaryEditor = () => {
+const DiaryEditor = ({onCreate}) => {
     const authorInput = useRef();
     const contentInput = useRef();
 
@@ -29,7 +29,13 @@ const DiaryEditor = () => {
             contentInput.current.focus();
             return;
         }
+        onCreate(state.author, state.content, state.emotion)
         alert("save success");
+        setState({
+            author: "",
+            content: "",
+            emotion: 1
+        })
     }
 
     return (
