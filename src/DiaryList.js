7140a386d59fa6935a64a7f78c71ca59322eea85
syckor <syckor@naver.com>
2023-12-13 22:13:05 +0900

    set css

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -8,7 +8,7 @@ const DiaryList = ({diaryList}) => {
             <h4>{diaryList.length}개의 일기가 있습니다.</h4>
             <div>
                 {diaryList.map((it, idx) => ( //만약 key로 쓸만한 값이 없다면 idx를 쓸 수 있지만 조심해야 한다.
-                <DiaryItem key={it.id} {...it}/>
+                    <DiaryItem key={it.id} {...it}/>
 
                 ))}
             </div>
