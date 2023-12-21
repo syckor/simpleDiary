af5970e399834bc2910f40e4c0f46e8389007fc3
syckor <syckor@naver.com>
2023-12-21 21:40:10 +0900

    memo optimize

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,38 +1,52 @@
 import React,{useState, useEffect} from "react";
 
-const TextView = ({text})=>{
-    useEffect(() => {
-        console.log(`Update :: text : ${text}`);
+const CounterA = React.memo(({count}) => {
+
+    useEffect(()=>{
+        console.log(`CounterA update - count : ${count}`)
     })
-    return <div>{text}</div>
+    return <div>{count}</div>;
+});
+
+const CounterB = ({obj}) => {
+    useEffect(()=>{
+        console.log(`CounterB update - count : ${obj.count}`)
+    })
+    return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps, nextProps) => {
+    if(prevProps.obj.count === nextProps.obj.count) {
+        return true;
+    }
+    return false;
 }
 
-const CountView = ({count}) => {
-    useEffect(() => {
-        console.log(`Update :: count : ${count}`);
-    })
-    return <div>{count}</div>
-}
+const MemoizedCounterB = React.memo(CounterB, areEqual);
 
 
 const OptimizeTest = () => {
 
-    const [count, setCount] = useState(1);
-    const [text, setText] = useState("");
+    const [count,setCount] = useState(1);
+    const [obj, setObj] = useState({
+        count : 1
+    });
 
-    return <div style={{ padding: 50 }}>
+    return (
+        <div style={{ padding: 50 }}>
             <div>
-                <h2>count</h2>
-                <CountView count={count} />
-                <button onClick={()=>setCount(count+1)}>+</button>
+                <h2>Counter A</h2>
+                <CounterA count={count} />
+                <button onClick={()=>setCount(count)}>A Button</button>
             </div>
             <div>
-                <h2>text</h2>
-                <TextView text={text} />
-                <input value={text} onChange={(e) => setText(e.target.value)}></input>
+                <h2>Counter B</h2>
+                <MemoizedCounterB obj={obj} />
+                <button onClick={()=>setObj({count:obj.count})}>A Button</button>
             </div>
 
-    </div>;
+        </div>
+    )
 };
 
 export default OptimizeTest;
